09da665554c9597d1e3438e8b1a8be86444bf67e
rasmuskstagsted@live.dk <rasmuskstagsted@live.dk>
2016-06-06 19:21:00 +0200

    Variable platform size

--- startOver.lua
+++ startOver.lua
@@ -1,13 +1,19 @@
+shell.run("functions")
+
+platformSize = 1
+
 gotoNewPlace()
 placeDirt()
-placeTree()
-chopTree()
-craftFloor()
-buildFloor()
 
 while (true) do
-	chopLeaves()
-	suckLeaves()
 	placeTree()
 	chopTree()
+	if (platformSize < 3) then
+		craftFloor()
+		if (getNumberOfItems("minecraft:planks") >= (platformSize + 2) * (platformSize + 2) - 1) then
+			expandPlatform(platformSize)
+			platformSize = platformSize + 1
+		end
+	chopLeaves()
+	suckLeaves(platformSize)
 end
